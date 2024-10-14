48ddc9a29e5fe3a4aa446d208a6a15d6b50742ab
anikamvp <anikavodicka@gmail.com>
2024-10-14 14:08:04 -0700

    fun stuff

--- js/site.js
+++ js/site.js
@@ -19,3 +19,18 @@ function main() {
 
 // let's get this party started
 main();
+/**
+ * Original Version + Artist :       
+ * https://codepen.io/rachelnabors/pen/bpAJH
+ * 
+ */
+
+$( document ).ready(function() {
+
+  // Change cat's animation form walking to sitting on completion of bed movement
+  var transitionPrefixes = 'animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd';
+    $('.cat-bed').one(transitionPrefixes, function(){
+      $('.lazy-cat').removeClass('walking');
+      $('.lazy-cat').addClass('sitting');		
+    });
+  });
