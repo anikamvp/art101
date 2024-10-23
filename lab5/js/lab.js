bba4b80ecb796179dcd2dccab1cdcf42b0039af3
anikamvp <anikavodicka@gmail.com>
2024-10-23 09:40:55 -0700

    yes

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,9 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Car data
+// Author: Anika Paredes
+// Date: Oct 22
 
-// Constants
+//Define Variables
 
+make = "Toyota";
+model = "Prius";
+color = "blue";
+year = 2012;
+
+//Calulate 
+age = 2024 - year;
+12
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
