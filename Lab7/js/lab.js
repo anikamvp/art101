8c9a04e96a41453f8d9acbe94f6810a9a3eab862
anikamvp <anikavodicka@gmail.com>
2024-11-04 14:06:29 -0800

    yes

--- Lab7/js/lab.js
+++ Lab7/js/lab.js
@@ -8,19 +8,18 @@ function sortUserName() {
     var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
 console.log("userName =", userName);
 // split string to array
-    var nameArray = userName.split('Anika');
+    var nameArray = userName.split('');
  console.log("nameArray =", nameArray);
  // sort the array
     var nameArraySort = nameArray.sort();
  console.log("nameArraySort =", nameArraySort);
  // join array back to a string
-    var nameSorted = nameArraySort.join('Anika');
+    var nameSorted = nameArraySort.join('');
  console.log("nameSorted =", nameSorted);
  
-// userName.tolower() ,lspl it("") .sort() .join("")
- return nameSorted;
+  return nameSorted;
  }
 
 //output
  document.writeln("Oh hey, I've fixed your name: ",
- sortUserName(), "<lbr>");
+ sortUserName(), "<Anika>");
