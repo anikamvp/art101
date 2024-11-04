706b6b0d85e73e05bab294758425dcb774aafa38
anikamvp <anikavodicka@gmail.com>
2024-11-04 14:44:20 -0800

    Update lab.js

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -19,6 +19,7 @@ document.writeln("Model: " + model + "<br>");
 document.writeln("Color: " + color + "<br>");
 document.writeln("Year: " + year + "<br>");
 document.writeln("Age: " + age + "years<br>");
+
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
