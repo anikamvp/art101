4c990d6411cc703058be1c0617350025c35f96bf
anikamvp <anikavodicka@gmail.com>
2024-11-05 12:17:43 -0800

    yes

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -2,7 +2,7 @@
 // Author: Anika
 // Date: Nov 5
 
- 
+ //test function
  function isEven(x){
    return (x % 2 == 0);
    }
@@ -12,11 +12,9 @@
     var array = [100, 81, 4, 16, 42, 144, 10000];
     console. log("My array", array);
 
-      var result= array.map(isEven);
-      console.log ("Test of evenness of array:", result);
-      var result= array.map(function(x){
-         return x ** 0.5;
+   var result= array.map(isEven);
+   console.log ("Test of evenness of array:", result);
+   var result= array.map(function(x){
+      return x ** 0.5;
       })
-
-      console.log("Squareroot of array:", result);
-      
+   console.log("Squareroot of array:", result);
