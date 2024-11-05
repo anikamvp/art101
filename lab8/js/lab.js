e12db8a7a31906a3d1b3d883b44b6d5beb551fa2
anikamvp <anikavodicka@gmail.com>
2024-11-05 11:58:37 -0800

    yes

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -9,7 +9,6 @@
     console.log("Is 1 even? ", isEven(1));
     console.log("Is 2 even? ", isEven(2));
 
-    array = [100, 81, 4, 16, 42, 144, 10000]
     var array = [100, 81, 4, 16, 42, 144, 10000];
     console. log("My array", array);
 
