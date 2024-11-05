353e6673d54c2985aa0bc3f184899a6d4a2f07ff
anikamvp <anikavodicka@gmail.com>
2024-11-05 13:12:53 -0800

    yes

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -8,6 +8,15 @@
    Requirements: jQuery must be loaded for this script to work.
 */
 $("#challenge").append("<button id='button-challenge'>Make Special</button>");
+$("#problems").append("<button id='button-challenge'>Make Special</button>");
+$("#results").append("<button id='button-challenge'>Make Special</button>");
+
 $("#button-challenge").click(function(){
   $("#challenge").toggleClass("special");
+});
+$("#button-challenge").click(function(){
+  $("#problems").toggleClass("special");
+});
+$("#button-challenge").click(function(){
+  $("#results").toggleClass("special");
 });
